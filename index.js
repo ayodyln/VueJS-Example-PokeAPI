fb1538ed41b7a99c18d9dfdacab71b3897f64a1a
ayodyln <78442693+ayodyln@users.noreply.github.com>
2023-02-17 19:47:05 -0700

    added sort, types, favorites

--- index.js
+++ index.js
@@ -9,17 +9,40 @@ createApp({
         next: "",
         prev: null,
       },
+      favorites: [],
+      filter: "",
+      typeFilter: [
+        "Normal",
+        "Fire",
+        "Water",
+        "Grass",
+        "Electric",
+        "Ice",
+        "Fighting",
+        "Poison",
+        "Ground",
+        "Flying",
+        "Psychic",
+        "Bug",
+        "Rock",
+        "Ghost",
+        "Dragon",
+        "Dark",
+        "Steel",
+      ],
     }
   },
   methods: {
     async fetchNextPokemon() {
       if (this.pageCount === 5) return
       this.pageCount++
+      this.filter = ""
       await this.fetchPokedex(this.pokedex.next)
     },
     async fetchPrevPokemon() {
       if (this.pageCount === 1) return
       this.pageCount--
+      this.filter = ""
       await this.fetchPokedex(this.pokedex.prev)
     },
     async fetchPokedex(
@@ -28,6 +51,7 @@ createApp({
       try {
         const data = await fetch(url)
         const pokemon = await data.json()
+
         this.pokedex.list = await this.fetchPokemonData(pokemon.results)
         this.pokedex.next = pokemon.next
         this.pokedex.prev = pokemon.previous
@@ -41,13 +65,41 @@ createApp({
           .map(async (pokemon) => {
             try {
               const data = await fetch(pokemon.url)
-              return await data.json()
+              const res = await data.json()
+              return {
+                name: res.name,
+                image: res.sprites.front_default,
+                id: res.id,
+                type: res.types.map((type) => type.type.name),
+                weight: res.weight,
+                favorite: this.favorites.find(
+                  (pokemon) => pokemon.name === res.name
+                ),
+              }
             } catch (error) {
               console.log(error)
             }
           })
           .sort((a, b) => a.id - b.id)
       ).then((val) => val)
+    },
+    toggleFavorite(event) {
+      const pokeID = event.target.dataset.id * 1
+      const pokemon = this.pokedex.list.find((pokemon) => pokemon.id === pokeID)
+      pokemon.favorite = !pokemon.favorite
+      this.favorites.push(pokemon)
+    },
+    logFavorites() {
+      console.log(this.favorites)
+    },
+    filterPokemon() {
+      this.pokedex.list = this.pokedex.list.filter((pokemon) =>
+        pokemon.type.includes(this.filter.toLowerCase())
+      )
+    },
+    async resetFilter() {
+      await this.fetchPokedex()
+      this.pageCount = 1
     },
   },
   async mounted() {
